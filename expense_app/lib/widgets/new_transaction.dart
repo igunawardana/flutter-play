ec3da3b8f80049381831c048eee9fe53fe5099e9
Isuru Gunawardana <igunawardana87@gmail.com>
2019-07-11 13:23:50 +0530

    SingleChildScrollView Scrollable

---
+++ expense_app/lib/widgets/new_transaction.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  // const NewTransaction({Key key}) : super(key: key);
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function addTx;
+
+  NewTransaction(this.addTx);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              controller: amountController,
+            ),
+            FlatButton(
+              child: Text('Add Transaction'),
+              textColor: Colors.purple,
+              onPressed: () {
+                print(titleController.text);
+                print(amountController.text);
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
