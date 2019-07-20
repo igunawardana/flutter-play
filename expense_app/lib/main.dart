d8bcb2fe8536e6ba9dc6dc0c2d13171761229220
Isuru Gunawardana <igunawardana87@gmail.com>
2019-07-20 22:54:59 +0530

    Stick the orientation to portrait

--- expense_app/lib/main.dart
+++ expense_app/lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import './models/transaction.dart';
 import './widgets/new_transaction.dart';
 import './widgets/tranaction_list.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
