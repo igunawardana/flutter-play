46f6b630a2f462556309b750f3b905094c8b8b3a
Isuru Gunawardana <igunawardana87@gmail.com>
2019-07-30 19:07:36 +0530

    Adding a switch

--- meals_app/lib/screens/filters_screen.dart
+++ meals_app/lib/screens/filters_screen.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = '/filters';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +22,32 @@ class FiltersScreen extends StatelessWidget {
         title: Text(''),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filter'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust Your meal selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                SwitchListTile(
+                  title: Text('Gluten-free'),
+                  value: _glutenFree,
+                  subtitle: Text('Only include gluten-free meals.'),
+                  onChanged: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
